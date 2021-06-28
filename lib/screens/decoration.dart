42fac99cbe92637d26f8973c7c4eee62bc66b5b4
Ghazal <ghazaal.laghaee@gmail.com>
2021-06-28 21:52:36 +0430

    final

--- lib/screens/decoration.dart
+++ lib/screens/decoration.dart
@@ -6,11 +6,12 @@ const buttonColor = const Color(0XFFd83e56);
 
 class myDecoration {
 
-  static InputDecoration TextFormFieldDecoration(String hintText, String labelText) {
+  static InputDecoration TextFormFieldDecoration(String hintText, String labelText,String error) {
     return InputDecoration(
       errorMaxLines: 2,
       hintText: hintText,
       labelText: labelText,
+      errorText: error,
       contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
       enabledBorder: OutlineInputBorder(
         borderSide: BorderSide(color: buttonColor, width: 1.0),
